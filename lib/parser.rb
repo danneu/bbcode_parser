5b6e2635831adb875d0ebd0e89a198039220fa38
Dan Neumann <danrodneu@gmail.com>
2012-11-17 01:56:34 -0600

    add printable_chars and ensure 'text' doesn't consume closing tags.

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,11 +2,14 @@ require 'parslet'
 
 class BBCode < Parslet::Parser
   TAGS = %w[b i u s font quote color left right center url]
-
+  TAGS.each do |bbcode|
+    rule(bbcode.to_sym) { str(bbcode) } 
+  end
   rule(:space) { str(' ').repeat(1) }
   rule(:space?) { space.maybe }
   rule(:newline) { (str("\r\n") | str("\n")) >> space? }
   rule(:whitespace) { (space | newline).repeat(1) }
+  rule(:whitespace?) { whitespace.maybe }
 
   # === BBCODE OPTIONS ===
   # [bbcode=a;b;c] => options: [option, option, option]
@@ -17,13 +20,13 @@ class BBCode < Parslet::Parser
   rule(:options?) { options.repeat(0, 1) }
 
   # === STRINGS - ALLOWED CHARACTERS ===
+  rule(:printable_char) { match['[:word:]'] | match['[:punct:]'] } # [Letters, numbers, underscores] [Punctuation and symbols]
   rule(:tag_options) { match['a-zA-Z0-9 '].repeat(1) }
   rule(:tag_name)    { match['a-zA-Z'].repeat(1) }
-  rule(:text){ (match["a-zA-Z\.\:\,\"\!\'"] | space).repeat(1).as(:text) | ((newline.as(:break) >> space?).repeat(1,2).as(:breaks) >> whitespace.maybe)  }
+  #rule(:text){ (match["a-zA-Z\.\:\,\"\!\'"] | space).repeat(1).as(:text) | ((newline.as(:break) >> space?).repeat(1,2).as(:breaks) >> whitespace.maybe)  }
+  #rule(:text){ (printable_char | space).repeat(1).as(:text) | ((newline.as(:break) >> space?).repeat(1,2).as(:breaks) >> whitespace.maybe)  }
+  rule(:text){ (close.absent? >> printable_char | space).repeat(1).as(:text) | ((newline.as(:break) >> space?).repeat(1,2).as(:breaks) >> whitespace.maybe)  }
 
-  TAGS.each do |bbcode|
-    rule(bbcode.to_sym) { str(bbcode) } 
-  end
   rule(:inline_tag) { b | i | url | u | s | color | font }
   rule(:block_tag) { quote | left | right | center }
   rule(:tag) { inline_tag | block_tag }
@@ -34,7 +37,7 @@ class BBCode < Parslet::Parser
   # This is because users don't intend to add linebreaks when they're just
   # spacing out their text after a block:
   #
-  # +---Example Post 1------------+
+  # +---Example Post 1------------+ OUTPUT -->
   # | [quote=Barack Obama]        | <div class="quote">
   # |   I am the president.       |   I am the president
   # | [/quote]                    | </div> <-- User didn't intend for additional vertical spacing 
@@ -55,13 +58,14 @@ class BBCode < Parslet::Parser
     
 
   #rule(:block) { (open >> (block | text >> (space | newline.repeat(1)).maybe).repeat.as(:inner) >> whitespace.maybe >> close) }
-  rule(:block) { (open >> (block | text).repeat.as(:inner) >> whitespace.maybe >> close) }
+  rule(:block) { (open >> (block | text).repeat.as(:inner) >> whitespace? >> close) }
   rule(:body) { (block | text).repeat.as(:body) }
   root(:body)
 
   def self.parse(str)
-    new.parse(str)
+    new.parse(str, reporter: Parslet::ErrorReporter::Deepest.new)
   rescue Parslet::ParseFailed => failure
     puts failure.cause.ascii_tree
+    "Parse Error: " << failure.cause.ascii_tree
   end
 end
